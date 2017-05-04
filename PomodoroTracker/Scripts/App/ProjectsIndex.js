590173ed9c8880eeeee443ac5fd78ca758ff10c0
Angel Hu <angelqhu513@gmail.com>
2017-05-04 11:25:32 -0500

    Functionality on the create page

---
+++ PomodoroTracker/Scripts/App/ProjectsIndex.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $('#NewProjectForm').on('submit', function (event) {
+        event.preventDefault();
+
+        var newProjectDescription = $('#NewProjectDescriptionInput').val();
+
+        //Post the new description to our API.
+        $.post('/api/projectsapi', { Description: newProjectDescription })
+            .done(function (data) {
+                var newProjectId = data.ProjectID;
+                var newProjectEditLink = '<a href="/Projects/Edit/' + newProjectId + '">Edit</a>';
+                var newProjectDetailsLink = '<a href="/Projects/Details/' + newProjectId + '">Details</a>';
+                var newProjectDeleteLink = '<a href="/Projects/Delete/' + newProjectId + '">Delete</a>';
+
+                $('.table tbody').append('<tr><td>' + newProjectDescription + '</td><td>' + newProjectEditLink + ' | ' + newProjectDetailsLink + ' | ' + newProjectDeleteLink + '</td ></tr >');
+                $('#NewProjectDescriptionInput').val('');
+                console.log(data);
+            });
+    });
+
+    //Attach a click event handler to #NewProjectCreateButton
+    $('#NewProjectCreateButton').on('click', function (event) {
+        //Get the value form #NewProjectDescriptionInput
+       
+    });
+});
