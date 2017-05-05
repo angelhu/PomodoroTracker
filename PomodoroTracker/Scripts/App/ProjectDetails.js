e773f3f11813720c7a09bca70765ecf5186c4676
Angel Hu <angelqhu513@gmail.com>
2017-05-05 11:59:41 -0500

    Updated the edit/details/delete button on the project task page

---
+++ PomodoroTracker/Scripts/App/ProjectDetails.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    var newProjectTaskRowTemplate = Handlebars.compile($('#NewProjectTaskRow').html());
+
+    $('table').on('click', 'a.command-button', function (event) {
+        event.preventDefault();
+        console.log(event, $(this).data('method'));
+
+        var method = $(this).data('method');
+
+        if (method === "delete") {
+            $.ajax({ url: $(this).attr('href'), type: "DELETE" }).done(function (data) {
+            $('#ProjectTaskRow-' + data.ProjectTaskID).remove();
+            });
+        }
+    });
+
+    $('#NewTaskForm').on('submit', function (event) {
+        event.preventDefault();
+
+        var url = $('#NewTaskForm').attr('action');
+
+        var newProjectTask = {
+            Description: $('#NewProjectTaskDescriptionInput').val(),
+            EstimatedPomodoroCount: $('#NewProjectTaskEstimatedPomodorosInput').val(),
+            ProjectID: $('#ProjectId').val()
+        }
+
+        $.post(url, newProjectTask).done(function (data) {
+            
+            console.log(data);
+            $('#NewProjectTaskDescriptionInput').val('');
+            $('#NewProjectTaskEstimatedPomodorosInput').val('');
+            $('.table tbody').append(newProjectTaskRowTemplate(data));
+
+        });
+
+    });
+});
